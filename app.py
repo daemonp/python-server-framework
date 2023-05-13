4b95775f8c69c1055de96c3bc65d920f9ad0ff29
Damon Petta <damon@petta.org>
2023-05-13 10:20:47 +0100

    properly structure the example application

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+"""
+Flask application to return a Hello World
+"""
+
 from flask import Flask
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-@app.route('/')
+
+@APP.route('/')
 def hello_world():
+    """
+    Return a Hello World
+    """
     return 'Hello, World!'
 
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    APP.run(debug=True, host='0.0.0.0', port=5000)
--- test_app.py
+++ test_app.py
@@ -1,5 +1,13 @@
+"""
+Unit test for app.py
+"""
+
 import pytest
 import app
 
+
 def test_hello_world():
+    """
+    Test hello_world function
+    """
     assert app.hello_world() == "Hello, World!"
